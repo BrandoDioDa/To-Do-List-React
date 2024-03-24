e6abcd669ca85357cbeb0fe69a16b4d2077c35b4
BrandoDioDa <stevenson.brandon01@gmail.com>
2024-03-24 18:36:37 -0500

    added base navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 
-const Navbar = () => {
+function Navbar (){
     return (
-        <>
-        
-        </>
+    <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+    </nav>
     );
-};
+}
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import Navbar from './components/Navbar'
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <h1 className="text-danger">Hello Bootstrap</h1>
-    </main>
+    <div>
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <main className={styles.main}>
+          <h1 className="text-danger">Hello Bootstrap</h1>
+      </main>
+    </div>
+   
   )
 }
