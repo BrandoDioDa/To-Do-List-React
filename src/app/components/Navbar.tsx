ceb5f2e93ca753479748212313a4bc1bda6ba237
BrandoDioDa <stevenson.brandon01@gmail.com>
2024-06-04 21:38:54 -0500

    Nav-bar sticks to top of screen

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 
 const Navbar = () => {
     return (
-    <nav className="navbar navbar-light bg-light flex-container">
+    <nav className="navbar fixed-top bg-light flex-container">
         <div className="container-fluid">
             <span className="navbar-brand mb-0 h1">Navbar</span>
         </div>
