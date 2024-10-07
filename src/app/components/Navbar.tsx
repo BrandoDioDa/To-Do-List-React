298086d0473fd396a86918fd03cb7695e5808314
Brandon Stevenson <brandon_stevenson01@hotmail.com>
2024-10-06 21:54:21 -0500

    Added personal title to navbar | working on hamburger logic

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,29 +2,31 @@ import React, {useState, useEffect} from "react";
 import style from '../style/navbar.module.css';
 
 const Navbar = () => {
-    return (
-    <div className="container fixed-top">
-        <span className="navbar-brand mb-0">
-            <img className={style.logo} src="/icon.ico"/>
-        </span> 
-        <nav className='navbar navbar-expand-lg '>
+    return (     
+        <nav className='navbar fixed-top navbar-expand-lg navbar-light bg-light'>
             <div className="container-fluid">
+                <div>
+                    <a className={`navbar-brand ${style.TwoPerLine}`} href="#">Brandon Stevenson</a>
+                    <p className={`${style.TitleFont} ${style.TwoPerLine}`}>Software Engineer</p>
+                </div>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                 <div className="collapse navbar-collapse row" id="navbarNav"> 
-                    <ul className="navbar-nav">
-                        <li className={`${style.navText} nav-item col`}>
+                    <ul className={`navbar-nav ${style.navLinks}`}>
+                        <li className="nav-item col">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li className={`${style.navText} nav-item col`}>
-                            <a className="nav-link" href="#">Resume</a>
+                        <li className="nav-item col">
+                            <a className="nav-link active" href="#">Resume</a>
                         </li>
-                        <li className={`${style.navText} nav-item col`}>
-                            <a className="nav-link" href="#" aria-disabled="true">Projects</a>
+                        <li className="nav-item col">
+                            <a className="nav-link active" href="#" aria-disabled="true">Projects</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-    </div>
     );
 };
 export default Navbar;
