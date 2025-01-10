dbf10dfebe46b751219e101d4ca8cf7575d96763
unknown <stevenson.brandon01@gmail.com>
2025-01-09 19:47:43 -0600

    fixed default size for navbar. Working on navbar changing style based off compressed vs stretched

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,18 +1,18 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import style from '../style/navbar.module.css';
 
 const Navbar = () => {
-    return (     
+    return (
         <nav className='navbar fixed-top navbar-expand-lg navbar-light bg-light'>
             <div className="container-fluid">
-                <div>
+                <div className={`${style.titlePosition}`}>
                     <a className={`navbar-brand ${style.TwoPerLine}`} href="#">Brandon Stevenson</a>
                     <p className={`${style.TitleFont} ${style.TwoPerLine}`}>Software Engineer</p>
                 </div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse row" id="navbarNav"> 
+                <div className="collapse navbar-collapse row" id="navbarNav">
                     <ul className={`navbar-nav ${style.navLinks}`}>
                         <li className={`'nav-item' ${style.col}`}>
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
